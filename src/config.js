e3644ad73cf2c54709b51a276e2a5f1f8bf0a132
Lakshay saini <lakshaysaini2013@gmail.com>
2021-02-19 13:03:47 +0530

    update

---
+++ src/config.js
@@ -0,0 +1,4 @@
+export const API_KEY = "https://www.thesportsdb.com/api/v1/json/1";
+// export const NEWS_API_KEY = "e38e540406e643bba69034aea4379fc7";
+export const NEWS_BASE_URL =
+  "https://saurav.tech/NewsAPI/top-headlines/category";
